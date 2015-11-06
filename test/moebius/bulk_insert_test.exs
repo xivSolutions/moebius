18b8c5c5cf186c207295d0de9fc894206241fba6
xivSolutions <xivSolutions@gmail.com>
2015-11-06 07:51:25 -0700

    arrest Tx with malformed inputs

--- test/moebius/bulk_insert_test.exs
+++ test/moebius/bulk_insert_test.exs
@@ -28,7 +28,7 @@ defmodule MoebiusBulkInsertTest do
   end
 
   test "bulk insert fails as a transaction" do
-    data = flawed_people(10000)
+    data = flawed_people(4)
     res = db(:people) |> insert(data)
     assert {:error, "null value in column \"first_name\" violates not-null constraint"} == res
     # no records were written to the db either...
@@ -46,6 +46,7 @@ defmodule MoebiusBulkInsertTest do
       ]))
   end
 
+  # tests for trans failures dur to constraint violations:
   defp flawed_people(qty) do
     p = Enum.reverse(people(qty - 1))
     flawed = [
@@ -58,5 +59,18 @@ defmodule MoebiusBulkInsertTest do
     ]
     Enum.reverse([flawed | p])
   end
+
+  # tests for trans failures due to malformed inputs:
+  # defp flawed_people(qty) do
+  #   p = Enum.reverse(people(qty - 1))
+  #   flawed = [
+  #     first_name: "X",
+  #     last_name: "Y",
+  #     address: "Z",
+  #     city: "fucked city",
+  #     state: "BumFuck",
+  #   ]
+  #   Enum.reverse([flawed | p])
+  # end
 
 end
